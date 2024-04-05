95d6a9fb983f79f350bed9f57bcb71ee584ce957
mathis47000 <mathisbescond3@gmail.com>
2024-04-05 10:49:03 +0200

    done

--- room.js
+++ room.js
@@ -28,11 +28,21 @@ socket.on('disconnect', () => {
 })
 
 
-socket.on('message', message => {
+socket.on('message', (message) => {
     // add message to chat
     const messageBox = '<div class="message-box">'
         + '<span class="pseudo">Alex</span>'
         + '<span class="message">' + message + '</span></div>'
     const messageContainer = document.querySelector('.message-container')
     messageContainer.innerHTML += messageBox
+})
+
+// send message to room
+
+const sendButton = document.querySelector('.send')
+
+sendButton.addEventListener('click', () => {
+    const message = document.querySelector('.input-message').value
+    socket.emit('message', { 'url': urlParams.get('room'), 'message': message })
+    document.querySelector('.input-message').value = ''
 })
